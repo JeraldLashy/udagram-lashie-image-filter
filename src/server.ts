b3bc23c11249488fb626fb36dfb5a5bf88dd0c15
Jerald Lashy Jeffery <jeraldjeffreylashie@gmail.com>
2022-09-04 19:41:00 +0200

    modified function to use return keyword to avoid bug

--- src/server.ts
+++ src/server.ts
@@ -14,17 +14,17 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
+  // Done == @TODO1 IMPLEMENT A RESTFUL ENDPOINT 
+  // Done == ET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  //  
+  //DONE    1. validate the image_url query  DONE
+  //DONE    2. call filterImageFromURL(image_url) to filter the image   DONE
+  //DONE   3. send the resulting file in the response   DONE
+  //DONE   4. deletes any files on the server on finish of the response DONE
   // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
+  //    image_url: URL of a publicly accessible image     DONE
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
@@ -35,10 +35,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     const image_url = req.query.image_url.toString();
 
     if(!image_url){
-      res.status(400).send('Please Note that the Image Url is reuired');
-
+      return res.status(400).send('Please Note that the Image Url is reuired');
     } 
-
     const filtered_image = await filterImageFromURL(image_url);
     res.status(200).sendFile(filtered_image, () => {deleteLocalFiles([filtered_image])});
     });
